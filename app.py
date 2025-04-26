38df1f804ba3699bd9409f2e0b17aeffc40c6f9a
victor.telles@iteso.mx <victor.telles@iteso.mx>
2025-04-25 18:21:43 -0600

    archivo de arranque

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from ui.components import main_layout
+
+# --- Punto de Entrada Principal ---
+if __name__ == "__main__":
+    main_layout()
+
+# Para ejecutar: abre tu terminal en el directorio 'camion-seguro-iot'
+# y corre el comando: streamlit run app.py
